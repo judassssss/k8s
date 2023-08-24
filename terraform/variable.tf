fdeffec8498db0744cabeab1a7dcd80902988dde
judassssss <vinhnt8@LAP13032.localdomain>
2023-08-24 13:35:08 +0700

    k8s

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,8 +1,8 @@
 variable "client_id" {
   type = string
-  default = "4203f6fb-a3e4-4005-b38f-450793b13233"
+  default = "4203f6fb-a3e4-4005-b38f-xxxxxxx"
 }
 variable "client_secret" {
   type = string
-  default = "0c964a48-55cf-4005-8527-93b786972dc0"
+  default = "0c964a48-55cf-4005-8527-xxxxxxx"
 }
